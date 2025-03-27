5bb998fe5796a395d713b8a1d3f6793b736a3d2b
Fahad Malik <fmalik07@nyit.edu>
2025-03-27 12:04:01 -0400

    Created Custom Cards

--- lib/widget/expenses.dart
+++ lib/widget/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,6 +11,20 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Cheeseburger',
+      amount: 11.99,
+      date: DateTime.now(),
+      category: Category.food
+    ),
+    Expense(
+      title: 'Movie Ticket',
+      amount: 18.99,
+      date: DateTime.now(),
+      category: Category.leisure
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
